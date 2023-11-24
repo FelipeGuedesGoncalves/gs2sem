ddf1957ef69592b09c44a62e5e025db0f95d887d
FelipeGuedesGoncalves <felipeguedes6559@gmail.com>
2023-11-24 10:47:29 -0300

    profissional enviando mensagens

--- src/app/ChatProfissional/[id]/page.jsx
+++ src/app/ChatProfissional/[id]/page.jsx
@@ -10,100 +10,107 @@ const philo = Philosopher({
 import { useEffect, useState } from 'react';
 
 export default function ChatProfissional({ params }) {
-    const idSala = params.id == 0 ? '' : params.id
-    const idMensagem = params.id == 0 ? '' : params.id
-    const [profissional, setProfissional] = useState({});
-    const [sala, setSala] = useState({});
-    const [mensagens, setMensagens] = useState([]);
-    const [novaMensagem, setNovaMensagem] = useState('');
+  const idSala = params.id == 0 ? '' : params.id
+  const idMensagem = params.id == 0 ? '' : params.id
+  const [profissional, setProfissional] = useState({});
+  const [sala, setSala] = useState({});
+  const [mensagens, setMensagens] = useState([]);
+  const [novaMensagem, setNovaMensagem] = useState('');
 
-    useEffect(() => {
-        // Obter dados do profissional da sessionStorage
-        const profissionalData = JSON.parse(sessionStorage.getItem('loginprof'));
+  useEffect(() => {
+      // Obter dados do profissional da sessionStorage
+      const profissionalData = JSON.parse(sessionStorage.getItem('loginprof'));
 
-        if (!profissionalData) {
-            // Se não houver dados do profissional, você pode redirecionar ou tratar de acordo com seus requisitos
-            console.error('Dados do profissional não encontrados na sessionStorage');
-            return;
-        }
-        setProfissional(profissionalData);
+      if (!profissionalData) {
+          // Se não houver dados do profissional, você pode redirecionar ou tratar de acordo com seus requisitos
+          console.error('Dados do profissional não encontrados na sessionStorage');
+          return;
+      }
+      setProfissional(profissionalData);
 
-        // Assumindo que o ID da sala está armazenado no profissionalData.id_sala (ajuste conforme necessário)
+      // Assumindo que o ID da sala está armazenado no profissionalData.id_sala (ajuste conforme necessário)
 
-        // Consultar o banco de dados para obter os detalhes da sala pelo ID
-        fetch(`http://localhost:3001/salas/${idSala}`, {
-            method: 'GET',
-        })
-            .then((resp) => resp.json())
-            .then((salaEncontrada) => {
-                if (salaEncontrada) {
-                    setSala(salaEncontrada);
-                    // Ordenar mensagens com base no número da ID
-                    const mensagensOrdenadas = salaEncontrada.mensagens.sort((a, b) => a.id - b.id);
-                    setMensagens(mensagensOrdenadas);
-                } else {
-                    // Caso não encontre a sala, você pode lidar com isso de acordo com seus requisitos
-                    console.error(`Não foi possível encontrar a sala com o ID ${idSala}`);
-                }
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    }, []);
+      // Consultar o banco de dados para obter os detalhes da sala pelo ID
+      fetch(`http://localhost:3001/salas/${idSala}`, {
+          method: 'GET',
+      })
+          .then((resp) => resp.json())
+          .then((salaEncontrada) => {
+              if (salaEncontrada) {
+                  setSala(salaEncontrada);
+                  // Ordenar mensagens com base no número da ID
+                  const mensagensOrdenadas = salaEncontrada.mensagens.sort((a, b) => a.id - b.id);
+                  setMensagens(mensagensOrdenadas);
+              } else {
+                  // Caso não encontre a sala, você pode lidar com isso de acordo com seus requisitos
+                  console.error(`Não foi possível encontrar a sala com o ID ${idSala}`);
+              }
+          })
+          .catch(error => {
+              console.error(error);
+          });
+  }, []);
 
-    const handleEnviarMensagem = async () => {
-        try {
+
+  const handleEnviarMensagem = async (event) => {
+    event.preventDefault();
+      try {
           // Cria um objeto representando a nova mensagem
           const novaMensagemObj = {
-            id: mensagens.length + 1, // Gera um ID único para a nova mensagem
-            conteudo: novaMensagem,
-            rm_profissional: profissional.rm_profissional,
+              id: mensagens.length + 1, // Gera um ID único para a nova mensagem
+              conteudo: novaMensagem,
+              rm_profissional: profissional.rm_profissional,
           };
-    
-          // Envia a nova mensagem para o backend usando o método POST
-          await fetch(`http://localhost:3001/salas/${idSala}/mensagens/${idMensagem}`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(novaMensagemObj),
+
+          // Atualiza a variável localmente com a nova mensagem
+          const mensagensAtualizadas = [...mensagens, novaMensagemObj];
+          setMensagens(mensagensAtualizadas);
+
+          // Atualiza as mensagens no servidor usando o método PUT
+          await fetch(`http://localhost:3001/salas/${idSala}`, {
+              method: 'PUT',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify({ mensagens: mensagensAtualizadas }),
           });
-    
-          // Recarrega a página para exibir a nova mensagem
-        } catch (error) {
+
+          // Limpa a caixa de mensagem após o envio
+          setNovaMensagem('');
+      } catch (error) {
           console.error('Erro ao enviar mensagem:', error);
-        }
-      };
-    
-      const renderizarMensagens = () => {
-        return mensagens.map((mensagem) => (
+      }
+  };
+
+  const renderizarMensagens = () => {
+      return mensagens.map((mensagem) => (
           <div
-            key={mensagem.id}
-            className={`mensagem ${mensagem.email ? 'mensagemCliente' : 'mensagemProfissional'}`}
+              key={mensagem.id}
+              className={`mensagem ${mensagem.email ? 'mensagemCliente' : 'mensagemProfissional'}`}
           >
-            {mensagem.conteudo}
+              {mensagem.conteudo}
           </div>
-        ));
-      };
-    
-      return (
-        <main className='mainChat'>
+      ));
+  };
+
+  return (
+      <main className='mainChat'>
           <h1 className={philo.className} id='titulo'>
-            Assistência Médica
+              Assistência Médica
           </h1>
-    
+
           <div className='cliente'></div>
           <div className='conversa'>
-            <div className='mensagens'>{renderizarMensagens()}</div>
+              <div className='mensagens'>{renderizarMensagens()}</div>
           </div>
           <form className='enviarMensagem'>
-            <input
-              className='escreverMsg'
-              value={novaMensagem}
-              onChange={(e) => setNovaMensagem(e.target.value)}
-            />
-            <button className='enviarmsg' onClick={handleEnviarMensagem}>
-              Enviar
-            </button>
+              <input
+                  className='escreverMsg'
+                  value={novaMensagem}
+                  onChange={(e) => setNovaMensagem(e.target.value)}
+              />
+              <button className='enviarmsg' onClick={handleEnviarMensagem}>
+                  Enviar
+              </button>
           </form>
-        </main>
-      );
-    }
+      </main>
+  );
+}
